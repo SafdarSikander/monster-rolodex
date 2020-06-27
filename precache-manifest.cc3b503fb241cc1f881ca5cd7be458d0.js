2e5993e8471a36e1818483da931dc2ff83791d2a
Safdar Sikander <safdar_sikander@live.com>
2020-06-27 13:05:45 +0500

    Updates

---
+++ precache-manifest.cc3b503fb241cc1f881ca5cd7be458d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2112453931ceb304a53b61e5d8008d5b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b24f431213fea88c7013",
+    "url": "/monsters-rolodex/static/css/main.ab8abf97.chunk.css"
+  },
+  {
+    "revision": "888e8789f6a6d96a40a4",
+    "url": "/monsters-rolodex/static/js/2.74a6cbd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.74a6cbd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24f431213fea88c7013",
+    "url": "/monsters-rolodex/static/js/main.40c270a6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
